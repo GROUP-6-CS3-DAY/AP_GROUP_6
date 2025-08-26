d274652d7bfd99974f246347dfa8e0ca5424d338
MulindwaMarkMyles <mylesmark753@gmail.com>
2025-08-26 18:53:11 +0300

    program and project model setup

---
+++ app/Models/Program.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Program extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'program_ID';
+
+    protected $fillable = [
+        'program_ID',
+        'name',
+        'description',
+        'national_alignment',
+        'focus_areas',
+        'phases'
+    ];
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class, 'program_ID');
+    }
+}
