21c294cc1fa25f937a6cec66de3fb8d867705c6e
SundayEmmanuelHezekiah <sundayemmanuelhezekiah@gmail.com>
2025-10-13 12:37:35 +0300

    creating tests for outcomes

--- app/Application/UseCases/DeleteOutcomeUseCase.php
+++ app/Application/UseCases/DeleteOutcomeUseCase.php
@@ -26,4 +26,23 @@ class DeleteOutcomeUseCase
 
         $this->outcomeRepository->delete($outcomeId);
     }
+
+    public function validateDeletion(string $outcomeId): void
+    {
+        $outcome = $this->outcomeRepository->findById($outcomeId);
+        
+        if (!$outcome) {
+            throw new OutcomeNotFoundException("Outcome with ID {$outcomeId} not found");
+        }
+
+        // Business rule validations
+        if ($outcome->getCommercializationStatus()->getValue() === 'commercialized') {
+            throw new \DomainException('Cannot delete commercialized outcomes');
+        }
+
+        // Additional business rules can be added here
+        if ($outcome->hasQualityCertification() && $outcome->getCommercializationStatus()->getValue() !== 'not_applicable') {
+            throw new \DomainException('Cannot delete certified outcomes that may be commercialized');
+        }
+    }
 }
